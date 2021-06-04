11edd8c6a1c7eb4d4df4f827509602724a4aba69
mariaangelesmt <angymontesdeocatoribio@gmail.com>
2021-06-04 15:31:41 +0100

    created home-user page with news and style

--- src/app/components/home-user/home-user.component.ts
+++ src/app/components/home-user/home-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/admin/news.service';
+import { TagsService } from 'src/app/services/user/tags.service';
 
 @Component({
   selector: 'app-home-user',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeUserComponent implements OnInit {
 
-  constructor() { }
+  news: any = []; //aqui tengo la noticia con todo su cuerpo
+
+  constructor(
+    private newsService: NewsService, 
+    private tagsService: TagsService 
+  ) { }
 
   ngOnInit(): void {
+    this.newsService.getAllNews().then(data => { this.news = data});
   }
 
 }
